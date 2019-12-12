555e5bbec816130b9d639016b1448680a17a7e9e
= <greg.o23@att.net>
2019-12-11 16:50:05 -0800

    added buttons that know whether you are in a group or not. need to get the insert command to work when the button is pressed.

--- pages/searchgroups.php
+++ pages/searchgroups.php
@@ -13,8 +13,14 @@
   <body>
 
     <?php
+      if(isset($_POST['Join'])){
+        joinbutton();
+      }
 
       include 'connectDB.php'; // has $conn=mysqli_connect(...) in it needs, mysqli_close() to end connection
+
+      $username = $_SESSION["username"];
+
       $query = "SELECT gID, gName, owner, numMembers
                 FROM Groups";
 
@@ -32,19 +38,45 @@
         echo "<div>Well apparently there are no more groups, which means something went very wrong </div>";
       }
 
+      $partOfGroup = "SELECT G.gName, G.owner, G.gID FROM Groups G, MemberOf M WHERE G.gID = M.gID AND '$username' = M.username";
+      //$buttonResult = mysqli_query($conn, $partOfGroup);
+
       while($row = mysqli_fetch_array($result)) {
+        $buttonResult = mysqli_query($conn, $partOfGroup);
         echo "<div class='card rounded'>";
           echo "<div class='card-body'>";
             echo "<a class='nav-link' href='grouppage.php?group=".$row['gName']."'>".$row['gName']."</a>";
           echo "</div>"; //end top;
           echo "<div class=card-footer>";
-                echo "Group admin: " . $row['owner'] . "\t|\t";
-                echo $row['numMembers'] . " Members";
+
+                echo " Group admin: " . $row['owner'] . "\t|\t";
+                echo $row['numMembers'] . " Members \t|\t" ;
+                $count = 0;
+                while($buttonif = mysqli_fetch_array($buttonResult)){
+                  if($buttonif['gName'] == $row['gName']){
+                    echo "<button onclick='joinbutton()' class='btn btn-outline-success'>Leave</button>";
+                    break;
+                  }
+                  else if($count == mysqli_num_rows($buttonResult)-1)
+                  {
+                    echo "<button onclick='joinbutton()' class='btn btn-outline-primary'>Join</button>";
+                    break;
+                  }
+                  $count++;
+                }
+                $count = 0;
+
           echo "</div>";
         echo "</div>";
+        mysqli_free_result($buttonResult);
       }
       echo "</div>";//end TorD
       echo "</div>"; //end container
+
+      function joinbutton() {
+        $sql = "INSERT INTO MemberOf (username, gID, num_pts) VALUES ('$username', '$row[gID]', '0')";
+        mysqli_query($conn, $sql);
+      }
 
       mysqli_free_result($result);
 
